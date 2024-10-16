14587627cee18094147312aab87486a26dfe5c0c
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-10-16 13:42:15 +0000

    adad

--- motorController.py
+++ motorController.py
@@ -44,7 +44,7 @@ class MotorController:
         if self.side == "L":
             rpm = -1*rpm
         
-        if self.power == 0.00:
+        if self.power <= 0.001:
             return
         
         self.power = self.pid(rpm, dt)
@@ -52,7 +52,7 @@ class MotorController:
 
         print(self.target_rpm)
         print(f"RPM: {rpm:.2f}, PID Output Power: {self.power:.5f}, Motor Throttle: {self.motor.throttle:.2f}")
-    
+
     def adjust_pid_constants(self, key):
         if key == 'z':
             self.K_P += 0.0001
