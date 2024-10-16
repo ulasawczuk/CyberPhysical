a5909680d665397ea93188516912fa37d07cc78c
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-10-16 13:35:53 +0000

    dada

--- motorController.py
+++ motorController.py
@@ -24,7 +24,7 @@ class MotorController:
         # PID constants
         self.K_P = 0.001
         self.K_I = 0.0001
-        self.K_D = 0
+        self.K_D = 0.0001
         self.pid = PID(self.K_P, self.K_I, self.K_D, setpoint=target_rpm)
         self.pid.output_limits = (-1, 1) 
 
