e8c76f941e64fc56bfb29e00bbbf5ebcbc9cb74e
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-10-23 13:53:02 +0000

    dad

--- colorSensor.py
+++ colorSensor.py
@@ -15,7 +15,5 @@ class ColorSensor:
             return "Red"
         elif r < 12 and b > 10 and g > 10:
             return "Blue"
-        elif r > 10 and b >= 0 and b < 10 and g > 10:
-            return "Black"
         else:
-            return "Unknown"
+            return "Black"
