893ec497f251da6ecae64580a672f253f51e8e7a
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-10-25 12:26:07 +0000

    trying to fix the i2c error

--- server.py
+++ server.py
@@ -105,7 +105,12 @@ while True:
 
             if followLine:
 
-                r, g, b = colorSensor.get_rgb()
+                try:
+                    r, g, b = colorSensor.get_rgb()
+                except OSError as e:
+                    print("I2C error, retrying in 0.1 seconds:", e)
+                    time.sleep(0.1)  # Small delay before retrying
+                    continue
                 current_color = colorSensor.classify_color(r, g, b)
                 print(f"red: {r}, green: {g}, blue: {b}")  
                 print(f"Color: "+ current_color)
