c109d8f31d26a83450610cd25e96c3a232838d6f
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-10-25 12:03:35 +0000

    bigger turn when blue or red

--- server.py
+++ server.py
@@ -132,7 +132,7 @@ while True:
                 # If red is detected, turn right to find black
                 elif current_color == "Red" and not turning_right:
                     print("Red tape detected, turning right.")
-                    motorL.update_target_rpm(2)  
+                    motorL.update_target_rpm(5)  
                     motorR.update_target_rpm(0) 
                     turning_right = True  
                     turning_left = False 
@@ -141,7 +141,7 @@ while True:
                 elif current_color == "Blue" and not turning_left:
                     print("Blue tape detected, turning left.")
                     motorL.update_target_rpm(0) 
-                    motorR.update_target_rpm(2)  
+                    motorR.update_target_rpm(5)  
                     turning_left = True  
                     turning_right = False 
 
