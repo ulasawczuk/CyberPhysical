0f0543965276606257107d86ab101acda477b7c6
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-11-05 22:13:14 +0000

    awa

--- server.py
+++ server.py
@@ -178,8 +178,8 @@ while True:
                     print("Red tape detected, turning right.")
                     #motorL.update_target_rpm(24)  
                     #motorR.update_target_rpm(9) 
-                    motorL.motor.throttle = 0.35
-                    motorR.motor.throttle = 0.1
+                    motorL.motor.throttle = 0.4
+                    motorR.motor.throttle = 0.0
                     turning_right = True  
                     turning_left = False 
                     #halfSecondColor = 0.1
@@ -189,8 +189,8 @@ while True:
                     print("Blue tape detected, turning left.")
                     #motorL.update_target_rpm(9) 
                     #motorR.update_target_rpm(24)  
-                    motorL.motor.throttle = 0.1
-                    motorR.motor.throttle = 0.35
+                    motorL.motor.throttle = 0.0
+                    motorR.motor.throttle = 0.4
                     turning_left = True  
                     turning_right = False
                     #halfSecondColor = 0.1
