ad770b89618310fc9f0f53f4b239c2792ce2b808
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-11-05 22:14:50 +0000

    wq

--- server.py
+++ server.py
@@ -168,8 +168,8 @@ while True:
                 elif current_color == "Black" and not turning_left and not turning_right and found:
                     #motorL.update_target_rpm(MOTOR_SPEED)
                     #motorR.update_target_rpm(MOTOR_SPEED)
-                    motorL.motor.throttle = 0.4
-                    motorR.motor.throttle = 0.4
+                    motorL.motor.throttle = 0.3
+                    motorR.motor.throttle = 0.3
                     found = False
                     print("Back on black tape, moving straight.")
 
