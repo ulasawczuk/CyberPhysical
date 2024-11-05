59724d6fecd3908e31b3c87434dde98c33d40d70
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-11-05 17:18:23 +0000

    esf

--- colorSensor.py
+++ colorSensor.py
@@ -11,15 +11,9 @@ class ColorSensor:
 
     def classify_color(self, r, g, b):
         # Classify the color based on the RGB values
-        if r > 20 and r > g + b:
-            if r < 40:
-                return "Blue"
-            elif r == 45 and g == 12 and b == 4 or r == 25 and g == 20 and b == 3 or r == 25 and g == 12 and b == 4:
-                return "Blue"
+        if r > g + b:
             return "Red"
-        elif r < 30 and b >= 3 and g > 10 and not r == g == b or r == 16 and g == 16 and b == 16:
+        elif b > r + g or b > 15:
             return "Blue"
-        elif r == g or r == g == b or r < 20 and b < 10 and g > 10:
-            return "Black"
         else:
-            return "Unknown"
+            return "Black"
