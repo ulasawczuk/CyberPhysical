a49f1d8ef53d9ecb6e595eb54db607ef90a3c524
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-11-05 16:45:45 +0000

    less turning

--- server.py
+++ server.py
@@ -161,7 +161,7 @@ while True:
                 # If red is detected, turn right to find black
                 elif current_color == "Red" and not turning_right:
                     print("Red tape detected, turning right.")
-                    motorL.update_target_rpm(28)  
+                    motorL.update_target_rpm(20)  
                     motorR.update_target_rpm(0) 
                     turning_right = True  
                     turning_left = False 
@@ -170,7 +170,7 @@ while True:
                 elif current_color == "Blue" and not turning_left:
                     print("Blue tape detected, turning left.")
                     motorL.update_target_rpm(0) 
-                    motorR.update_target_rpm(28)  
+                    motorR.update_target_rpm(20)  
                     turning_left = True  
                     turning_right = False 
 
